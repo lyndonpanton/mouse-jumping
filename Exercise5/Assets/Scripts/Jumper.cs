d5e0609448435401565cd06d6631cbdf40c9a649
Lyndon Panton <LyndonPanton@gmail.com>
2023-01-10 06:46:56 +0000

    Blue slime now cannot leave the game window

--- Exercise5/Assets/Scripts/Jumper.cs
+++ Exercise5/Assets/Scripts/Jumper.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 /// <summary>
@@ -7,6 +8,8 @@ using UnityEngine;
 /// </summary>
 public class Jumper : MonoBehaviour
 {
+    float colliderHalfWidth;
+    float colliderHalfHeight;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,10 @@ public class Jumper : MonoBehaviour
         scale.y *= 4;
 
         transform.localScale = scale;
+
+        BoxCollider2D bd2 = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = (bd2.size.x / 2) * 4;
+        colliderHalfHeight = (bd2.size.y / 2) * 4;
     }
 
     // Update is called once per frame
@@ -28,6 +35,37 @@ public class Jumper : MonoBehaviour
             pixelMousePosition.z = -Camera.main.transform.position.z;
 
             Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(pixelMousePosition);
+
+            Vector3 worldSizeVector = Camera.main.ScreenToWorldPoint(
+                new Vector3(
+                    Camera.main.pixelWidth,
+                    Camera.main.pixelHeight,
+                    worldMousePosition.z)
+            );
+
+            Debug.Log(worldSizeVector);
+            Debug.Log(worldMousePosition);
+
+            float minX = -worldSizeVector.x + colliderHalfWidth;
+            float maxX = worldSizeVector.x - colliderHalfWidth;
+            float minY = -worldSizeVector.y + colliderHalfHeight;
+            float maxY = worldSizeVector.y - colliderHalfHeight;
+
+            if (worldMousePosition.x <= minX)
+            {
+                worldMousePosition.x = minX;
+            } else if (worldMousePosition.x >= maxX)
+            {
+                worldMousePosition.x = maxX;
+            }
+
+            if (worldMousePosition.y <= minY)
+            {
+                worldMousePosition.y = minY;
+            } else if (worldMousePosition.y >= maxY)
+            {
+                worldMousePosition.y = maxY;
+            }
 
             transform.position = worldMousePosition;
         }
